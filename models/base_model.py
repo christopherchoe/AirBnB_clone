eed363101616cf78aaac1b91fba5beb42b111cbe
AlexYu01 <tengalexyu@gmail.com>
2019-02-15 00:28:36 +0000

    Call FileStorage methods from BaseModels

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 """
 from datetime import datetime
 import uuid
-from base_model import storage
+from models import storage
 
 
 class BaseModel:
@@ -25,10 +25,10 @@ class BaseModel:
                 elif key is not "__class__":
                     self.__dict__[key] = value
         else:
-            storage.new()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """
@@ -41,8 +41,8 @@ class BaseModel:
         """
             Updates `updated_at`.
         """
-        storage.save()
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
