24c3cf520ea3f9f00d826ae2417a5f4b1b92b908
christopherchoe <chchchoe@gmail.com>
2019-02-16 21:16:11 +0000

    added classes with respective class attributes

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Module containing the ``Review`` class
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+        The ``Review`` class which inherits from ``BaseModel`` class.
+    """
+
+
+    place_id = ""
+    user_id = ""
+    text = ""
