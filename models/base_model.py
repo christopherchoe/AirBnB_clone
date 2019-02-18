05a6087cdcf7d32df8ecaa31b1eb998942fefafb
christopherchoe <chchchoe@gmail.com>
2019-02-18 22:21:52 +0000

    changed circular import issue

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """
     Module containing the ``BaseModel`` class.
 """
+import models
 from datetime import datetime
 import uuid
 
@@ -34,7 +35,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of ``BaseModel`` instance."""
@@ -62,7 +63,7 @@ class BaseModel:
         """Updates `updated_at`."""
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing instance dict, instance __class__,
