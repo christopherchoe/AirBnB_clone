6a329d73955697114e9e1cfb1e237454ecbcb89a
AlexYu01 <tengalexyu@gmail.com>
2019-02-21 00:48:19 +0000

    Added missing tests
    Co-authored-by: christopherchoe <chchchoe@gmail.com>

--- tests/test_console.py
+++ tests/test_console.py
@@ -225,6 +225,60 @@ class TestConsole(unittest.TestCase):
         cmp_str = '** instance id missing **\n'
         self.assertNotEqual(cmp_str, self._last_write())
 
+    def test_show_10(self):
+        """Test show with BaseModel"""
+        cli = self.create()
+        cli.onecmd('create BaseModel')
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('BaseModel.show({})'.format(o_id))
+        self.assertIn(o_id, self._last_write())
+
+    def test_show_11(self):
+        """Test show with State"""
+        cli = self.create()
+        cli.onecmd('create State')
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('State.show({})'.format(o_id))
+        self.assertIn(o_id, self._last_write())
+
+    def test_show_12(self):
+        """Test show with City"""
+        cli = self.create()
+        cli.onecmd('create City')
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('City.show({})'.format(o_id))
+        self.assertIn(o_id, self._last_write())
+
+    def test_show_13(self):
+        """Test show with Amenity"""
+        cli = self.create()
+        cli.onecmd('create Amenity')
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Amenity.show({})'.format(o_id))
+        self.assertIn(o_id, self._last_write())
+
+    def test_show_14(self):
+        """Test show with Place"""
+        cli = self.create()
+        cli.onecmd('create Place')
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Place.show({})'.format(o_id))
+        self.assertIn(o_id, self._last_write())
+
+    def test_show_15(self):
+        """Test show with Review"""
+        cli = self.create()
+        cli.onecmd('create Review')
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Review.show({})'.format(o_id))
+        self.assertIn(o_id, self._last_write())
+
     def test_destroy_0(self):
         """Test show command without arguments"""
         cli = self.create()
@@ -286,6 +340,61 @@ class TestConsole(unittest.TestCase):
         cli.onecmd("User.destroy(\"{}\")".format(id_user[:-1]))
         self.assertEqual('', self._last_write())
 
+    def test_destroy_9(self):
+        """Test destroy with BaseModel"""
+        cli = self.create()
+        cl = 'BaseModel'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.destroy({})'.format(cl, o_id))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertEqual('** no instance found **\n', self._last_write())
+
+    def test_destroy_10(self):
+        """Test destroy with State"""
+        cli = self.create()
+        cl = 'State'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.destroy({})'.format(cl, o_id))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertEqual('** no instance found **\n', self._last_write())
+
+    def test_destroy_12(self):
+        """Test destroy with Place"""
+        cli = self.create()
+        cl = 'Place'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.destroy({})'.format(cl, o_id))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertEqual('** no instance found **\n', self._last_write())
+
+    def test_destroy_13(self):
+        """Test destroy with Amenity"""
+        cli = self.create()
+        cl = 'Amenity'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.destroy({})'.format(cl, o_id))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertEqual('** no instance found **\n', self._last_write())
+
+    def test_destroy_14(self):
+        """Test destroy with Review"""
+        cli = self.create()
+        cl = 'Review'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.destroy({})'.format(cl, o_id))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertEqual('** no instance found **\n', self._last_write())
+
     def test_all_0(self):
         """Test all command without arguments"""
         cli = self.create()
@@ -302,6 +411,118 @@ class TestConsole(unittest.TestCase):
         cli.onecmd("all")
         self.assertEqual(str(test_list), self._last_write()[:-1])
 
+    def test_all_1(self):
+        """Test all with BaseModel"""
+        cli = self.create()
+        cli.onecmd('create BaseModel')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('BaseModel.all()')
+        self.assertIn('[BaseModel]', self._last_write())
+
+    def test_all_2(self):
+        """Test all with Review"""
+        cli = self.create()
+        cli.onecmd('create Review')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Review.all()')
+        self.assertIn('[Review]', self._last_write())
+
+    def test_all_3(self):
+        """Test all with User"""
+        cli = self.create()
+        cli.onecmd('create User')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('User.all()')
+        self.assertIn('[User]', self._last_write())
+
+    def test_all_4(self):
+        """Test all with State"""
+        cli = self.create()
+        cli.onecmd('create State')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('State.all()')
+        self.assertIn('[State]', self._last_write())
+
+    def test_all_5(self):
+        """Test all with City"""
+        cli = self.create()
+        cli.onecmd('create City')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('City.all()')
+        self.assertIn('[City]', self._last_write())
+
+    def test_all_6(self):
+        """Test all with Amenity"""
+        cli = self.create()
+        cli.onecmd('create Amenity')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Amenity.all()')
+        self.assertIn('[Amenity]', self._last_write())
+
+    def test_all_7(self):
+        """Test all with Place"""
+        cli = self.create()
+        cli.onecmd('create Place')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Place.all()')
+        self.assertIn('[Place]', self._last_write())
+
+    def test_count_0(self):
+        """Test count with BaseModel"""
+        cli = self.create()
+        cli.onecmd('create BaseModel')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('BaseModel.count()')
+        self.assertEqual('1\n', self._last_write())
+
+    def test_count_1(self):
+        """Test count with User"""
+        cli = self.create()
+        cli.onecmd('create User')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('User.count()')
+        self.assertEqual('1\n', self._last_write())
+
+    def test_count_2(self):
+        """Test count  with State"""
+        cli = self.create()
+        cli.onecmd('create State')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('State.count()')
+        self.assertEqual('1\n', self._last_write())
+
+    def test_count_3(self):
+        """Test count with Place"""
+        cli = self.create()
+        cli.onecmd('create Place')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Place.count()')
+        self.assertEqual('1\n', self._last_write())
+
+    def test_count_4(self):
+        """Test count  with City"""
+        cli = self.create()
+        cli.onecmd('create City')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('City.count()')
+        self.assertEqual('1\n', self._last_write())
+
+    def test_count_5(self):
+        """Test count with Amenity"""
+        cli = self.create()
+        cli.onecmd('create Amenity')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Amenity.count()')
+        self.assertEqual('1\n', self._last_write())
+
+    def test_count_6(self):
+        """Test count with Review"""
+        cli = self.create()
+        cli.onecmd('create Review')
+        self.mock_stdout.reset_mock()
+        cli.onecmd('Review.count()')
+        self.assertEqual('1\n', self._last_write())
+
     def test_update_0(self):
         """Test update on BaseModel with valid input"""
         cli = self.create()
@@ -312,44 +533,156 @@ class TestConsole(unittest.TestCase):
         cli.onecmd('show BaseModel {}'.format(id_base))
         self.assertIn("'email': 'wow'", self._last_write()[:-1])
 
-    # basemode.all()
-    # review.all()
-# user.all()
-# state.all()
-#city.all()
-#amenity.all()
-#place.all()
-#basemodel.count()
-#user.count()
-#state.count()
-#place.count()
-#city.count()
-    #amenity.count()
-#review.count()
-#basemodel.show("id")
-#state.show..
-#city.show...
-#amenity.show...
-#place.show...
-    #review.show..
-#basemodel.destroy("id")
-    #state.destroy
-#place.destroy...
-#amenity.destroy...
-#review.destroy..
-#basemodel.update("id", "attr", "string")
-    #user.update..
-#state.update
-    #city.update
-    #place.update
-    #amenity.update
-#review.update
-#basemodel.update dict
-#user.update dict
-    # state.update dict
-    #amenity.update ..
-    #city.update..
-#place.update...
-    #review.update..
+    def test_update_1(self):
+        """Test update with BaseModel"""
+        cli = self.create()
+        cl = 'BaseModel'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {}, {})'.format(cl, o_id, 'hi', 'there'))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
 
+    def test_update_2(self):
+        """Test update with User"""
+        cli = self.create()
+        cl = 'User'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {}, {})'.format(cl, o_id, 'hi', 'there'))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
 
+    def test_update_3(self):
+        """Test update with State"""
+        cli = self.create()
+        cl = 'State'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {}, {})'.format(cl, o_id, 'hi', 'there'))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_4(self):
+        """Test update with City"""
+        cli = self.create()
+        cl = 'City'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {}, {})'.format(cl, o_id, 'hi', 'there'))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_5(self):
+        """Test update with Place"""
+        cli = self.create()
+        cl = 'Place'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {}, {})'.format(cl, o_id, 'hi', 'there'))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_6(self):
+        """Test update with Amenity"""
+        cli = self.create()
+        cl = 'Amenity'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {}, {})'.format(cl, o_id, 'hi', 'there'))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_7(self):
+        """Test update with Review"""
+        cli = self.create()
+        cl = 'Review'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {}, {})'.format(cl, o_id, 'hi', 'there'))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_8(self):
+        """Test update() with BaseModel"""
+        cli = self.create()
+        cl = 'BaseModel'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {})'.format(cl, o_id, "{ 'hi': 'there' }"))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_9(self):
+        """Test update() with User"""
+        cli = self.create()
+        cl = 'User'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {})'.format(cl, o_id, "{ 'hi': 'there' }"))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_10(self):
+        """Test update() with State"""
+        cli = self.create()
+        cl = 'State'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {})'.format(cl, o_id, "{ 'hi': 'there' }"))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_11(self):
+        """Test update() with Amenity"""
+        cli = self.create()
+        cl = 'Amenity'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {})'.format(cl, o_id, "{ 'hi': 'there' }"))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_12(self):
+        """Test update() with City"""
+        cli = self.create()
+        cl = 'City'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {})'.format(cl, o_id, "{ 'hi': 'there' }"))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_13(self):
+        """Test update() with Place"""
+        cli = self.create()
+        cl = 'Place'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {})'.format(cl, o_id, "{ 'hi': 'there' }"))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
+
+    def test_update_14(self):
+        """Test update() with Review"""
+        cli = self.create()
+        cl = 'Review'
+        cli.onecmd('create {}'.format(cl))
+        o_id = self._last_write()[:-1]
+        self.mock_stdout.reset_mock()
+        cli.onecmd('{}.update({}, {})'.format(cl, o_id, "{ 'hi': 'there' }"))
+        cli.onecmd('{}.show({})'.format(cl, o_id))
+        self.assertIn("'hi': 'there'", self._last_write())
